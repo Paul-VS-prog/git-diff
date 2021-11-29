1e07f0fa618a3ab376572dd7cede505852f21f24
Paul-VS-prog <74456837+Paul-VS-prog@users.noreply.github.com>
2021-11-29 22:28:38 +0300

    /

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println();
+        System.out.println();
         System.out.println("Hello World and Java");
         System.out.println(1);
         System.out.println(2);
